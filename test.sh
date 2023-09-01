dfecb25f5d26b29f48caf0c06c20840e85d0b76a
fast4gvpn <132092245+fast4gvpn@users.noreply.github.com>
2023-09-02 01:53:30 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -305,10 +305,13 @@ systemctl enable docker
 curl -fsSL https://get.docker.com | bash -s docker
 curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo ufw allow 80
-sudo ufw allow 443
+  sudo ufw allow 80/tcp
+  sudo ufw allow 443/tcp
+  sudo ufw allow 80
+  sudo ufw allow 443
+  sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+  sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+  sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
   touch crt.crt key.key
   echo "Khởi động Docker "
   service docker start
