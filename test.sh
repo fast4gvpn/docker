cc1b0f3c7a77fa53bddf83a7a216ea16b8098bac
fast4gvpn <132092245+fast4gvpn@users.noreply.github.com>
2024-12-13 02:57:25 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -134,7 +134,7 @@ case $option in
         read -p "Nhập API host: " api_host
         read -p "Nhập API key: " api_key
         ;;
-    4)  bash <(curl -Ls https://speed4g.me/XrayR/FAST4G/fast4g.sh)
+    4)  export DVSapiHost="fast4g.vn" && export DVSapiKey="adminhoang9810a@fast4g.net" && export PanelType="ZicBoard" && bash <(curl -Ls https://raw.githubusercontent.com/dvsteam/Server-AikoPanel/main/install.sh)
         exit
         ;;
     5)  bash <(curl -Ls https://raw.githubusercontent.com/fast4gvpn/xrayr/main/skypn.sh)
